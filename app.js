bcc941162e6e58996882b19b14e5bcd1ac3750e5
ATimaru <malmsteen99@gmail.com>
2018-06-01 23:09:57 +0300

    Added  functionality to Add button.
    Added  functionality to Add button.

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-//JS code goes here
+//Budget controller
+var budgetController = (function() {
+
+})();
+
+//UI budgetController
+var UIController = (function() {
+
+})();
+
+//Global app controller
+var controller = (function(budgetCtrl, UIctrl) {
+
+  var ctrlAddItem = function() {
+
+    //1. get the field input data
+
+    //2. ghe the add item to the budget budgetController
+
+    //3. add the new item to the UI
+
+    //4. calculate the budget
+
+    //5. display the budget on the UI
+    console.log('it works');
+  }
+
+  $('.add__btn').click(ctrlAddItem);
+
+  $(document).keypress(function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
+  });
+
+})(budgetController, UIController);
