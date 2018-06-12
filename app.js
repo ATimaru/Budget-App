1afe7bfd7e4366e72c26d787d313596194565a69
ATimaru <malmsteen99@gmail.com>
2018-06-12 22:58:47 +0300

    Actual budget calculation

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function() {
     this.value = value;
   };
 
+  var calculateTotal = function(type) {
+    var sum = 0;
+    data.allItems[type].forEach(function(cur) {
+      sum += cur.value;
+    });
+    data.totals[type] = sum;
+  };
+
   var data = {
     allItems: {
       exp: [],
@@ -21,7 +29,9 @@ var budgetController = (function() {
     totals: {
       exp: 0,
       inc: 0
-    }
+    },
+    budget: 0,
+    percentage: -1
   };
 
   return {
@@ -49,11 +59,37 @@ var budgetController = (function() {
       //Return the element
       return newItem;
     },
+
+    calculateBudget: function() {
+
+      //calculate total income and expenses
+      calculateTotal('exp');
+      calculateTotal('inc');
+
+      //calculate the budget: income - expenses
+      data.budget = data.totals.inc - data.totals.exp;
+
+      //calculate the percentage of income that we spent
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+          data.percentage = -1;
+      }
+    },
+
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      }
+    },
+
     testing: function () {
       console.log(data);
     }
   };
-
 })();
 
 //UI Controller
@@ -134,10 +170,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var updateBudget = function() {
     //1. calculate the budget
+    budgetCtrl.calculateBudget();
 
     //2. return the budget
+    var budget = budgetCtrl.getBudget();
 
     //3. display the budget on the UI
+    console.log(budget);
   };
 
   var ctrlAddItem = function() {
@@ -145,7 +184,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     //1. get the field input data
     input = UICtrl.getinput();
 
-    if (input.description !== '' && !isNaN(input.Value) && input.Value > 0) {
+    if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
       //2. ghe the add item to the budget budgetController
       newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
