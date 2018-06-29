e5a83a1bc3488b02c5db5e28db83a8518efb0d43
ATimaru <malmsteen99@gmail.com>
2018-06-29 23:31:17 +0300

    Improved UX.

--- app.js
+++ app.js
@@ -172,6 +172,12 @@ var UIController = (function() {
     return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
   };
 
+  var nodeListForEach = function(list, callback) {
+    for (var i = 0; i < list.length; i++) {
+      callback(list[i], i);
+    }
+  };
+
   return  {
     getinput: function() {
       return {
@@ -241,12 +247,6 @@ var UIController = (function() {
     displayPercentages: function(percentages) {
       var fields = $(DOMstrings.expensesPercLabel);
 
-      var nodeListForEach = function(list, callback) {
-        for (var i = 0; i < list.length; i++) {
-          callback(list[i], i);
-        }
-      };
-
       nodeListForEach(fields, function(current, index){
         if(percentages[index] > 0) {
           current.textContent = percentages[index] + '%';
@@ -263,8 +263,18 @@ var UIController = (function() {
       month = now.getMonth();
       year = now.getFullYear();
       $(DOMstrings.dateLabel).text(months[month] + ' ' + year);
-
     },
+
+    changedType: function() {
+      var fields;
+      fields = $(DOMstrings.inputType + ',' + DOMstrings.inputDescription + ',' + DOMstrings.inputValue);
+
+    nodeListForEach(fields, function(cur) {
+      cur.classList.toggle('red-focus');
+    });
+    document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
+  },
+
 
     getDOMstrings: function() {
       return DOMstrings;
@@ -285,6 +295,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
 
     $(DOM.container).click(ctrlDeleteItem)
+    $(DOM.inputType).change(UICtrl.changedType);
 
   };
 
